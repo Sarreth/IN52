bb17c14705739a79f66eb4a170c93498549d44ab
Bilb <audric.bilb@gmail.com>
2013-12-08 15:28:25 +0100

    suppression paused inutile

--- dev/vehicule_detection/main.cpp
+++ dev/vehicule_detection/main.cpp
@@ -69,9 +69,9 @@ int main()
 
     testCamShift();
 
-//    templateMatching();
-//    histogramEqua();
-//    testCamShift();
+    //    templateMatching();
+    //    histogramEqua();
+    //    testCamShift();
 
 
     destroyAllWindows();
@@ -171,40 +171,40 @@ Mat DFF(string path)
     tmp.copyTo(q2);
 
     normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-                                            // viewable image form (float between values 0 and 1).
+    // viewable image form (float between values 0 and 1).
     return I;
 }
 
 void MatchingMethod( int, void* )
 {
     /// Create the result matrix
-     int result_cols =  img.cols - templ.cols + 1;
-     int result_rows = img.rows - templ.rows + 1;
+    int result_cols =  img.cols - templ.cols + 1;
+    int result_rows = img.rows - templ.rows + 1;
 
-     result.create( result_cols, result_rows, CV_32FC1 );
+    result.create( result_cols, result_rows, CV_32FC1 );
 
-     /// Do the Matching and Normalize
-     matchTemplate( img, templ, result, match_method );
-     normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+    /// Do the Matching and Normalize
+    matchTemplate( img, templ, result, match_method );
+    normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
 
-     /// Localizing the best match with minMaxLoc
-     double minVal; double maxVal; Point minLoc; Point maxLoc;
-     Point matchLoc;
+    /// Localizing the best match with minMaxLoc
+    double minVal; double maxVal; Point minLoc; Point maxLoc;
+    Point matchLoc;
 
-     minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+    minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
 
-     /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-     if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
-       { matchLoc = minLoc; }
-     else
-       { matchLoc = maxLoc; }
+    /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+    if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
+    { matchLoc = minLoc; }
+    else
+    { matchLoc = maxLoc; }
 
-     /// Show me what you got
-     rectangle( imgOri, matchLoc*RESIZE_VAL, Point( matchLoc.x*RESIZE_VAL + templ.cols*RESIZE_VAL , matchLoc.y*RESIZE_VAL + templ.rows*RESIZE_VAL ), Scalar(0,0,255), 2, 8, 0 );
+    /// Show me what you got
+    rectangle( imgOri, matchLoc*RESIZE_VAL, Point( matchLoc.x*RESIZE_VAL + templ.cols*RESIZE_VAL , matchLoc.y*RESIZE_VAL + templ.rows*RESIZE_VAL ), Scalar(0,0,255), 2, 8, 0 );
 
-     imshow( image_window, imgOri );
+    imshow( image_window, imgOri );
 
-     return;
+    return;
 }
 
 int histogramEqua()
@@ -246,7 +246,7 @@ int testCamShift()
 
 
     Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
-    bool paused = false;
+
 
     string fn =  IMG_FILENAME;
     frame = imread(fn);
@@ -267,82 +267,80 @@ int testCamShift()
     for(;;)
     {
 
-        if( !paused )
-        {
-            frame = imread(nextFrameFilename);
-            count++;
-            if(frame.empty()) {
-                count = 3700;
-            }
 
-            stringstream ss;
-            ss << count;
-            nextFrameFilename = prefix + "W_" + ss.str() + "R" + suffix;
-            if(count == 3700) {
-                frame = imread(nextFrameFilename);
-            }
-
+        frame = imread(nextFrameFilename);
+        count++;
+        if(frame.empty()) {
+            count = 3700;
         }
+
+        stringstream ss;
+        ss << count;
+        nextFrameFilename = prefix + "W_" + ss.str() + "R" + suffix;
+        if(count == 3700) {
+            frame = imread(nextFrameFilename);
+        }
+
+
 
         frame.copyTo(image);
 
-        if( !paused )
+
+        cvtColor(image, hsv, CV_BGR2HSV);
+
+        if( trackObject )
         {
-            cvtColor(image, hsv, CV_BGR2HSV);
+            int _vmin = vmin, _vmax = vmax;
 
-            if( trackObject )
+            inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+            int ch[] = {0, 0};
+            hue.create(hsv.size(), hsv.depth());
+            mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+            if( trackObject < 0 )
             {
-                int _vmin = vmin, _vmax = vmax;
+                Mat roi(hue, selection), maskroi(mask, selection);
+                calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+                normalize(hist, hist, 0, 255, CV_MINMAX);
 
-                inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
-                int ch[] = {0, 0};
-                hue.create(hsv.size(), hsv.depth());
-                mixChannels(&hsv, 1, &hue, 1, ch, 1);
+                trackWindow = selection;
+                trackObject = 1;
 
-                if( trackObject < 0 )
+                histimg = Scalar::all(0);
+                int binW = histimg.cols / hsize;
+                Mat buf(1, hsize, CV_8UC3);
+                for( int i = 0; i < hsize; i++ )
+                    buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
+                cvtColor(buf, buf, CV_HSV2BGR);
+
+                for( int i = 0; i < hsize; i++ )
                 {
-                    Mat roi(hue, selection), maskroi(mask, selection);
-                    calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
-                    normalize(hist, hist, 0, 255, CV_MINMAX);
-
-                    trackWindow = selection;
-                    trackObject = 1;
-
-                    histimg = Scalar::all(0);
-                    int binW = histimg.cols / hsize;
-                    Mat buf(1, hsize, CV_8UC3);
-                    for( int i = 0; i < hsize; i++ )
-                        buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
-                    cvtColor(buf, buf, CV_HSV2BGR);
-
-                    for( int i = 0; i < hsize; i++ )
-                    {
-                        int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
-                        rectangle( histimg, Point(i*binW,histimg.rows),
-                                   Point((i+1)*binW,histimg.rows - val),
-                                   Scalar(buf.at<Vec3b>(i)), -1, 8 );
-                    }
+                    int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+                    rectangle( histimg, Point(i*binW,histimg.rows),
+                               Point((i+1)*binW,histimg.rows - val),
+                               Scalar(buf.at<Vec3b>(i)), -1, 8 );
                 }
-
-                calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
-                backproj &= mask;
-                RotatedRect trackBox = CamShift(backproj, trackWindow,
-                                    TermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1 ));
-                if( trackWindow.area() <= 1 )
-                {
-                    int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
-                    trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
-                                       trackWindow.x + r, trackWindow.y + r) &
-                                  Rect(0, 0, cols, rows);
-                }
-
-                if( backprojMode )
-                    cvtColor( backproj, image, CV_GRAY2BGR );
-                ellipse( image, trackBox, Scalar(0,0,255), 3, CV_AA );
             }
+
+            calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+            backproj &= mask;
+            RotatedRect trackBox = CamShift(backproj, trackWindow,
+                                            TermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1 ));
+            if( trackWindow.area() <= 1 )
+            {
+                int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
+                trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+                                   trackWindow.x + r, trackWindow.y + r) &
+                        Rect(0, 0, cols, rows);
+            }
+
+            if( backprojMode )
+                cvtColor( backproj, image, CV_GRAY2BGR );
+            ellipse( image, trackBox, Scalar(0,0,255), 3, CV_AA );
+
         }
-        else if( trackObject < 0 )
-            paused = false;
+
+
 
         if( selectObject && selection.width > 0 && selection.height > 0 )
         {
@@ -364,13 +362,11 @@ int testCamShift()
             trackObject = 0;
             histimg = Scalar::all(0);
             break;
-        case 'p':
-            paused = !paused;
-            break;
         default:
             ;
         }
     }
+
 
     return 0;
 }
