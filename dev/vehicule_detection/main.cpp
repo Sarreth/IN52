b0fc84acc98ab53e9c39975b6fda37fc1dff868c
Bilb <audric.bilb@gmail.com>
2013-12-12 16:29:25 +0100

    clean code 2

--- dev/vehicule_detection/main.cpp
+++ dev/vehicule_detection/main.cpp
@@ -181,17 +181,17 @@ int histogramEqua()
 int testCamShift(Rect selection)
 {
 
-    Mat image;
-    bool backprojMode = false;
-    int trackObject = -1;
+    Mat image, subimage, frame, hsv,
+            hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
 
-    int vmin = 100, vmax = 256, smin = 75;
     Rect trackWindow, rectCamshift;
-    Mat subimage;
-    int hsize = 16;
+
+    bool backprojMode = false;
+    int vmin = 100, vmax = 256, smin = 75, hsize = 16, count=3700, trackObject = -1;
+
     float hranges[] = {0,180};
     const float* phranges = hranges;
-
+    string prefix, suffix, nextFrameFilename;
 
     namedWindow( WINDOW_NAME, CV_WINDOW_AUTOSIZE );
 
@@ -201,10 +201,6 @@ int testCamShift(Rect selection)
     createTrackbar( "Smin", WINDOW_NAME, &smin, 256, 0 );*/
 
 
-    Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
-
-    string prefix, suffix, nextFrameFilename;
-    int count=3700;
     getSuffixAndPrefix(IMG_FILENAME, suffix, prefix);
 
     while(true)
